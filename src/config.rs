69d16312d41cccc216012c1fdaf8ff1f9e1d8fae
silentEAG <1223610721@qq.com>
2022-07-26 14:36:11 +0800

    init

--- src/config.rs
+++ src/config.rs
@@ -1,15 +1,11 @@
 #![allow(dead_code)]
 
-use std::{collections::HashMap, env::Vars, fmt::Display};
 use once_cell::sync::Lazy;
+use std::{collections::HashMap, env::Vars, fmt::Display};
 
-pub static CONFIG: Lazy<Config> = Lazy::new(||{
-    Config::load()
-});
+pub static CONFIG: Lazy<Config> = Lazy::new(Config::load);
 
-pub struct Config {
-
-}
+pub struct Config {}
 
 impl Config {
     pub fn load() -> Self {
@@ -31,10 +27,7 @@ impl ConfigBuilder {
             items.insert(key, value);
         });
         let cfg_type = "env".to_string();
-        ConfigBuilder { 
-            items,
-            cfg_type
-        }
+        ConfigBuilder { items, cfg_type }
     }
     pub fn from_file() -> Self {
         todo!()
@@ -51,8 +44,8 @@ impl ConfigBuilder {
 
 impl Display for ConfigBuilder {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.items.iter().for_each(|(key, value)|{
-            write!(f, "{} => {}\n", key, value).expect("Failed to display ConfigBuilder.");
+        self.items.iter().for_each(|(key, value)| {
+            writeln!(f, "{} => {}", key, value).expect("Failed to display ConfigBuilder.");
         });
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,20 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// Name of the person to greet
-   #[clap(short, long, value_parser)]
-   name: String,
+    /// Name of the person to greet
+    #[clap(short, long, value_parser)]
+    name: String,
 
-   /// Number of times to greet
-   #[clap(short, long, value_parser, default_value_t = 1)]
-   count: u8,
+    /// Number of times to greet
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    count: u8,
 }
 
 fn main() -> Result<()> {
-//    let args = Args::parse();
-//    for _ in 0..args.count {
-//        println!("Hello {}!", args.name);
-//    }
+    //    let args = Args::parse();
+    //    for _ in 0..args.count {
+    //        println!("Hello {}!", args.name);
+    //    }
     let env_builder = config::ConfigBuilder::from_env();
     println!("{}", env_builder);
     Ok(())
